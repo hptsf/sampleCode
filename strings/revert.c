f5317b18914ac306e4bc75538c05032ece1620ae
hptsf <hptsfb@gmail.com>
2017-06-14 21:42:54 -0400

    add o(n) method and test code

--- strings/revert.c
+++ strings/revert.c
@@ -11,6 +11,46 @@
 
 static char *pBuf = NULL;
 
+/* time consuming: O(n) */
+static char *do_revert2(const char *buf)
+{
+    int i;
+    int j;
+    int len;
+    char *pbuf = NULL;
+
+    if(NULL == buf)
+        return NULL;
+
+    len = strlen(buf);
+    pbuf = (char *)malloc(sizeof(char) * (len + 1));
+    if(NULL == pbuf){
+        perror("malloc failed");
+        return NULL;
+    }
+    memset(pbuf, 0x00, len + 1);
+
+    i = len - 1;
+    while(i >= 0 && ' ' == buf[i])       // delete space at end of String
+        i --;
+
+    j = 0;
+    while(i >= 0){
+        pbuf[j] = buf[i];
+        j ++;
+        i --;
+        while((i - 1) >= 0 && ' ' == buf[i] && ' ' == buf[i - 1])
+            i--;
+        if(0 == i && ' ' == buf[i])     // dealwith the first space
+            break;
+    }
+
+    pbuf[j] = '\0';
+
+    return pbuf;
+}
+
+/* time consuming: O(2n) */
 static char *do_revert(const char *buf)
 {
     int i;
@@ -28,6 +68,7 @@ static char *do_revert(const char *buf)
         perror("malloc failed");
         return NULL;
     }
+    memset(pbuf, 0x00, len + 1);
 
     i = 0;
     j = len;
@@ -62,14 +103,21 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    fprintf(stdout, "%s\n", argv[1]);
+    fprintf(stdout, "%s.\n", argv[1]);
     pBuf = do_revert(argv[1]);
     if(NULL == pBuf){
         fprintf(stdout, "revert failed\n");
         return 0;
     }
+    fprintf(stdout, "%s.\n", pBuf);
+    free(pBuf);
 
-    fprintf(stdout, "%s\n", pBuf);
+    pBuf = do_revert2(argv[1]);
+    if(NULL == pBuf){
+        fprintf(stdout, "revert failed\n");
+        return 0;
+    }
+    fprintf(stdout, "%s.\n", pBuf);
     free(pBuf);
 
     return 0;
